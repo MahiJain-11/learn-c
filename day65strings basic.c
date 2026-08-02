d55253f68453de93fb89405d85cb07417c8e58d3
Mahi jain <jainaryan976@gmail.com>
2026-08-02 22:27:05 +0530

    Add files via upload

---
+++ day65strings basic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+//STRINGS (character array)
+// A string is simply a sequence of characters placed together to form text.
+// C doesn't have a separate string data type. Instead, a string is stored 
+//as an array of characters ending with a special character called
+//the null character ('\0').
+
+//INITIALIZING
+// char arr[5] = {'a','t','l','a','s'};
+// printf("%c",arr[3]);
+
+//ASCII valuee
+//A = 65; a= 97 ;
+//0 = 48 ; 9 = 57;
+// char ch = 'A';
+// printf("%d",ch);
+// char ph = 'a';
+// printf("%d",ph);
+// char ch = 'A';
+// printf("%d",ch);
+    return 0;
+}
