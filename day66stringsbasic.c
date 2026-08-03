11a0036872269d4c29538815c34a5c1838e0de83
Mahi jain <jainaryan976@gmail.com>
2026-08-03 09:04:33 +0530

    Add files via upload

---
+++ day66stringsbasic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    //ASCII valuee
+// A = 65; a= 97 ;
+// 0 = 48 ; 9 = 57;
+// char ch = 'A';
+// printf("%d",ch);
+// char ph = 'a';
+// printf("%d",ph);
+// char ch = 'A';
+// printf("%d",ch);
+// char ch = '3';
+// int x = (int)ch;
+// printf("%d",ch);
+
+// CREATING CHARACTER ARAY (basically store the text)
+//suppose we need to store hello world
+//METHOD 1
+// char arr[11]={'h','e','l','l','o','w','o','r','l','d'};
+// for (int i = 0 ; i<11 ;  i++){
+//     printf("%c",arr[i]); 
+// }
+//METHOD 2 NULL CHARACTER ----->> /0
+// char arr[11]={'h','e','l','l','o','w','o','r','l','d'};
+// char ch = '\0'; 
+//null character...whose ascii value is 0
+// printf("%c",ch);
+
+    return 0;
+}
