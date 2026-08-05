55739627fa65268dc49984d3e794a2ea43be2d5e
Mahi jain <jainaryan976@gmail.com>
2026-08-05 09:15:41 +0530

    Add files via upload

---
+++ day68revstring.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    // How to take input
+    // char str[100];
+    // printf("Enter the input\n");
+    // // scanf("%s",&str);//only one word can be printed 
+    // fgets(str,100,stdin);//print whole sentencee
+    // printf("Your input is : %s",str);
+    
+    //take input from the user and print the reverse of string
+    // char arr[100];
+    // puts("enter a string");
+    // fgets(arr,sizeof(arr),stdin);
+    //puts automatically \0 lga deta hai
+    //reversing
+    // int k = 0;
+    // int size=0;
+    // while(arr[k]!= '\0'){
+    // size++;
+    // k++;
+    // }
+    // for(int i = 0 , j= size-1 ; i<=j ; i++, j--){
+    //     char temp = arr[i];
+    //     arr[i]= arr[j];
+    //     arr[j]=temp;
+    // }
+    // puts("The reverse string is: ");
+    // puts(arr);
+    return 0;
+}
