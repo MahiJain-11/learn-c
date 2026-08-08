d1533fff54afc317376616686cffd382c9c3a7f5
Mahi jain <jainaryan976@gmail.com>
2026-08-08 08:44:07 +0530

    Add files via upload

---
+++ day71stringssques.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    //WAP TO INSERT A NEW CHARACTER IN THE STRING TAKEN FROM USER
+    // char str[100];
+    // char ch;
+    // int pos;
+    // puts("Enter the string:");
+    // fgets(str,100,stdin);
+    // printf("enter the character:\n");
+    // scanf("%c",&ch);
+    // printf("enter the position:\n");
+    // scanf("%d",&pos);
+    // int len = strlen(str);
+    // for(int i = len; i>=pos; i--){
+    //     str[i+1]=str[i];
+    // }
+    // str[pos]=ch;
+    // printf("New string is: %s",str);
+    
+    return 0;
+}
