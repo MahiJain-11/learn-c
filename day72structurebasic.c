4153482aac1da36860ec2859bd736dffc1ef8500
Mahi jain <jainaryan976@gmail.com>
2026-08-08 09:41:26 +0530

    Add files via upload

---
+++ day72structurebasic.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+struct student{
+    char name[30];
+    int age;
+    float marks;
+};
+
+int main()
+{
+    //STRUCTURESS
+    //structure (struct) is used when we want to store
+    //different types of data together under one name.
+    //like if we want int char and float together.
+    //syntax:-
+    //struct structure_name {
+    //     data_type member1;
+    //     data_type member2;
+    //     data_type member3;
+    // };
+    
+    // int main() {
+    //     struct structure_name variable_name;
+    // }
+    // struct student s1 ={"Mahi",18,45.7};
+    // printf("Name = %s\n", s1.name);
+    // printf("Age = %d\n", s1.age);
+    // printf("Marks = %.2f\n", s1.marks);
+    
+    
+    return 0;
+}
