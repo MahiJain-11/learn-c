bdecf915f1273ff661de607c34e1e73730e93f98
Mahi jain <jainaryan976@gmail.com>
2026-08-17 08:58:41 +0530

    Add files via upload

---
+++ day79unionsbasic1.c
@@ -0,0 +1,30 @@
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    // // practising palindrome in structure
+    // char str[100];
+    // int i = 0;
+    // int j = 0;
+    // printf("Enter string\n");
+    // scanf("%s",str);
+    // while (str[j] != '\0'){
+    //     j++;
+    // }
+    // j--;
+    // while(i<j){
+    //     if(str[i] != str[j]){
+    //     printf("the word is not palindrome\n");
+    //     return 0;
+    //     }
+        
+    //     i++;
+    //     j--;
+    // }
+    // printf("palindrome\n);
+    
+    //UNIONS
+    
+    return 0;
+}
