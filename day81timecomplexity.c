2d124adea6d98f50dbb73f7b143da4a9e1ee245d
Mahi jain <jainaryan976@gmail.com>
2026-08-19 09:10:50 +0530

    Add files via upload

---
+++ day81timecomplexity.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+// sorting 
+//practise question
+// given an array  of integer with 1 to n elements and the size of the array if
+// n+1.One element is ocurring more than once i.e duplicate number present. find it
+
+//method 1
+// int arr[11] = {6,1,7,3,2,5,4,8,9,9,10};
+// int n = sizeof(arr)/sizeof(arr[0]);
+// for(int i = 0; i<n; i++){
+//     for(int j = i+1; j<n; j++){
+//         if(arr[i]==arr[j]){
+//             printf("%d is repeated",arr[i]);
+//             break;
+//         }
+//     }
+// }
+
+//TIME COMPLEXITY in terms of big O notation
+//Time complexity is NOT equal to time taken ...IT is rate at which time 
+//increases with respect to input size 
+// BIG O notation ---> O(time taken) ---> basically time complexity btata hai ki
+// kitne operations lg rhe hai 1 task ko krne main
+//So we want a way to describe how the work grows when input size n grows.
+// common complexities
+//0(1) = constant
+
+
+    return 0;
+}
