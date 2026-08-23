224f6f841ddd3488b84b6e79419d23405cc0fd33
Mahi jain <jainaryan976@gmail.com>
2026-08-23 09:10:57 +0530

    Add files via upload

---
+++ day85macro_macros_switch.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define PI 3.14159265
+float area(float r){
+    return PI*r*r;
+}
+int main()
+{
+//#include is preprossesor
+//stdio.h stands for standard(std) input(i) output(o) header(.h)
+// macros and macro function
+// #define is macros
+// printf("%f",PI);
+// printf("%.2f",area(1.5));
+
+// calculator using switch
+// char c;
+// printf("Enter operation\n");
+// scanf("%c",&c);
+// int a,b;
+// printf("enter two numbers\n");
+// scanf("%d %d",&a , &b);
+// switch(c){
+// case '+':
+//     printf("\n%d",a+b);
+//     break;
+// case '-':
+//     printf("\n%d",a-b);
+//     break;
+// case '*':
+//     printf("\n%d",a*b);
+//     break;
+// case '/':
+//     printf("\n%d",a/b);
+//     break;
+// }
+
+
+    return 0;
+}
