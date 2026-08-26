ece3f35b4b4348284a64b22af794db96544f5ef4
Mahi jain <jainaryan976@gmail.com>
2026-08-26 08:14:01 +0530

    Add files via upload

---
+++ day88boolean.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+//boolean and comparision
+// bool value1 = true; //  return 1 represent true
+// bool value2 = false; // return 2 represent false
+// printf("%d\n",value1);
+// printf("%d",value2);
+
+return 0;
+}
